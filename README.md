174cf985a0f38f90e067ff8f8c2b4295b952f4a1
Engels Tercero <engelstercero@skiff.com>
2022-07-09 18:57:15 -0600

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# WetterGarten
-WetterGarten is a SwiftUI new brand app for getting all weather data from your location, built with CoreLocation, URLSession and SwiftUI for your device
---
+++ WetterStation/WetterStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WetterStationApp.swift
+//  WetterStation
+//
+//  Created by Higashikata Maverick on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WetterStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
